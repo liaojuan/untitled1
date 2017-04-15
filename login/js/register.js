6ad0ef0de277f1e69a93d2f0b98b8f03fa013576
liaoruiwen <liaorw@dibugroup.cn>
2017-04-15 17:24:52 +0800

    在次抽一个public.js出来方便公用的，不然 写到register.js里面，不方便维护

--- login/js/register.js
+++ login/js/register.js
@@ -17,8 +17,6 @@ var isAgreeLaw = true;
 
 $(function () {
 
-    //TODO:对应的是注册页面
-
     //点击返回和返回前的图标
     $('#back_img_id').click(function () {
         window.close();
@@ -105,44 +103,7 @@ $(function () {
 
 });
 
-/**
- * 这个方法对应的所以获取验证码的button，type == 1,代表的是注册
- *  判断电话号码是否正确
- * @param str
- */
-function checkMonile(str, v, type) {
-    var type = type;
-    if(str == ""){
-        alert("手机号码不能为空！")
-    }else{
-        var rePhoneNum = /^1[3|4|5|7|8]\d{9}$/
-        if (rePhoneNum.test(str)){
-            //电话号码争取
-            $('#' + v.id).attr('disabled', false);
-            $('#' + v.id).css('color','#ffffff').css('background', '#ff6636');
-            if(type == 1){
-                checkOnclickButton();
-            }
-        }else {
-            alert("请输入正确电话号码");
-            $('#' + v.id).attr('disabled', true);
-        }
-    }
-}
 
-/**
- * 判断button键是否可以点击
- * 对应于注册页面
- */
-function checkOnclickButton() {
-    if($('#input_phone_num').val().length == 11 && $('#input_sms_code').val().length == 6 && $('#input_password_num').val().length > 5 && isAgreeLaw){
-        $('#sure_register_button').css('color','#ffffff').css('background', '#ff6636');
-        $('#sure_register_button').attr('disabled', false);
-    }else{
-        $('#sure_register_button').css('color','#ffffff').css('background', '#bfbfbf');
-        $('#sure_register_button').attr('disabled', true);
-    }
-}
 
 /**
  * 创建发送验证码参数
