25623c9d9fb2b2ceccbfb606f7d1fed9ecb4dfdd
liaoruiwen <liaorw@dibugroup.cn>
2017-04-15 17:15:59 +0800

    完成对forget_password.html样式公用，对register.js更进一步修改

--- login/js/register.js
+++ login/js/register.js
@@ -16,6 +16,9 @@ var isShow=true;
 var isAgreeLaw = true;
 
 $(function () {
+
+    //TODO:对应的是注册页面
+
     //点击返回和返回前的图标
     $('#back_img_id').click(function () {
         window.close();
@@ -84,7 +87,8 @@ $(function () {
             $('#sms_code_button').attr('disabled', true);
             checkOnclickButton();
         }else if(thisInput.length == 11){
-            checkMonile(thisInput);
+            var v = document.getElementById("sms_code_button");
+            checkMonile(thisInput, v, 1);
         }else if(thisInput.length > 11){
             alert("手机号码不能超过11位！");
             checkOnclickButton();
@@ -97,31 +101,38 @@ $(function () {
     $("#input_password_num").bind('input porpertychange',function(){
         checkOnclickButton();
     });
+
+
 });
 
 /**
+ * 这个方法对应的所以获取验证码的button，type == 1,代表的是注册
  *  判断电话号码是否正确
  * @param str
  */
-function checkMonile(str) {
+function checkMonile(str, v, type) {
+    var type = type;
     if(str == ""){
         alert("手机号码不能为空！")
     }else{
         var rePhoneNum = /^1[3|4|5|7|8]\d{9}$/
         if (rePhoneNum.test(str)){
             //电话号码争取
-            $('#sms_code_button').attr('disabled', false);
-            $('#sms_code_button').css('color','#ffffff').css('background', '#ff6636');
-            checkOnclickButton();
+            $('#' + v.id).attr('disabled', false);
+            $('#' + v.id).css('color','#ffffff').css('background', '#ff6636');
+            if(type == 1){
+                checkOnclickButton();
+            }
         }else {
             alert("请输入正确电话号码");
-            $('#sms_code_button').attr('disabled', true);
+            $('#' + v.id).attr('disabled', true);
         }
     }
 }
 
 /**
  * 判断button键是否可以点击
+ * 对应于注册页面
  */
 function checkOnclickButton() {
     if($('#input_phone_num').val().length == 11 && $('#input_sms_code').val().length == 6 && $('#input_password_num').val().length > 5 && isAgreeLaw){
